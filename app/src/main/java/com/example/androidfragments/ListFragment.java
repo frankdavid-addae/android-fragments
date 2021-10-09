07ca357b72dbcb85e1946a6dfaacff4abedf8677
Frank David Addae <frankdavid.addae@gmail.com>
2021-10-09 12:36:18 +0000

    device orientation

--- app/src/main/java/com/example/androidfragments/ListFragment.java
+++ app/src/main/java/com/example/androidfragments/ListFragment.java
@@ -1,6 +1,7 @@
 package com.example.androidfragments;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +18,7 @@ import java.util.ArrayList;
 public class ListFragment extends androidx.fragment.app.ListFragment {
 
     ItemSelected activity;
+    boolean dualPane;
 
     public interface ItemSelected {
         void onItemSelected(int index);
@@ -40,10 +42,14 @@ public class ListFragment extends androidx.fragment.app.ListFragment {
 //        data.add("1. This is item 1");
 //        data.add("2. This is item 2");
 //        data.add("3. This is item 3");
-        String [] pieces;
-        pieces = getResources().getStringArray(R.array.pieces);
+        String [] pieces = getResources().getStringArray(R.array.pieces);
 //        setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, data));
         setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, pieces));
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            activity.onItemSelected(0);
+        }
+
     }
 
     @Override
--- app/src/main/java/com/example/androidfragments/MainActivity.java
+++ app/src/main/java/com/example/androidfragments/MainActivity.java
@@ -1,21 +1,70 @@
 package com.example.androidfragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity implements ListFragment.ItemSelected {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        Fragment detailFragment = fragmentManager.findFragmentById(R.id.detailFragment);
+        Fragment listFragment = fragmentManager.findFragmentById(R.id.listFragment);
+
+        // Check if the phone is in portrait mode
+//        if (findViewById(R.id.layout_portrait) != null) {
+//            fragmentTransaction.hide(detailFragment);
+//            fragmentTransaction.show(listFragment);
+//            fragmentTransaction.commit();
+//        }
+
+        // Check if the phone is in landscape mode
+//        if (findViewById(R.id.layout_landscape) != null) {
+//            fragmentTransaction.show(detailFragment);
+//            fragmentTransaction.show(listFragment);
+//            fragmentTransaction.commit();
+//        }
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            fragmentTransaction.show(detailFragment);
+            fragmentTransaction.show(listFragment);
+            fragmentTransaction.commit();
+        }
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            fragmentTransaction.hide(detailFragment);
+            fragmentTransaction.show(listFragment);
+            fragmentTransaction.commit();
+        }
+
+
     }
 
     @Override
     public void onItemSelected(int index) {
         DetailFragment detailFragment = DetailFragment.newInstance(index);
-      getSupportFragmentManager().beginTransaction().replace(R.id.detailFragment, detailFragment)
+        getSupportFragmentManager().beginTransaction().replace(R.id.detailFragment, detailFragment)
                .commit();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        Fragment detailFrag = fragmentManager.findFragmentById(R.id.detailFragment);
+        Fragment listFragment = fragmentManager.findFragmentById(R.id.listFragment);
+
+        // Check if the phone is in portrait mode
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            fragmentTransaction.show(detailFragment);
+            fragmentTransaction.hide(listFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
     }
 }
